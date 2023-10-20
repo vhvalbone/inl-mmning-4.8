6abd8dc2a683df91d9a753d9b7c65c6aad335bcb
alsv06003 <alsv06003@E-PW0234JW>
2023-10-20 13:08:33 +0200

    Add project files.

---
+++ inlämmning 4.8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace inlämmning_4._8
+{
+    class program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Ange triangelns sidlängd");
+            int sidlängd = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < sidlängd; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
